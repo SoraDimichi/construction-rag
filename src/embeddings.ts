8f911dc51364ba70851a766d6fda9454878f9d0e
Dmitry Sorokin <dms@everum.eu>
2025-03-27 01:55:19 +0600

    before making fair vector store

--- src/embeddings.ts
+++ src/embeddings.ts
@@ -4,8 +4,6 @@ import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { config } from "./config.ts";
 import { Document } from "@langchain/core/documents";
 
-console.log(`${config.serviceUrl}/ml/v1/text/embeddings`);
-
 export const embeddings = new WatsonxEmbeddings({
   ...config,
   model: "ibm/slate-125m-english-rtrvr",
@@ -13,15 +11,17 @@ export const embeddings = new WatsonxEmbeddings({
 
 const fileContent = readFileSync("./result.json", "utf-8");
 const jsonData: Record<string, string> = JSON.parse(fileContent);
-const chunkedDocs: any = Object.entries(jsonData).map(
+const docs: any = Object.entries(jsonData).map(
   ([id, pageContent]) =>
     new Document({
-      pageContent: pageContent.slice(0, 999),
+      pageContent,
       id,
     }),
 );
 
-const vectorstore = MemoryVectorStore.fromDocuments(chunkedDocs, embeddings)
+const cutoff = docs.splice(0, 100);
+
+MemoryVectorStore.fromDocuments(cutoff, embeddings)
   .then((vectorStore) => vectorStore.asRetriever(1))
   .then((retriever) => retriever.invoke("What is LangChain?"))
   .then((returnedDocuments) => console.log(returnedDocuments[0].pageContent))
